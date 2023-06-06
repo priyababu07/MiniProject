2f583b019d531b624efd800680e4338e5d124200
priyababu07 <priyababu312@gmail.com>
2023-06-06 19:03:48 +0530

    Changes in table view

--- AdminDashboard/registration/pgw.php
+++ AdminDashboard/registration/pgw.php
@@ -1,4 +1,3 @@
-<!-- index.php -->
 
 <?php
 require_once 'config/db.php';
@@ -11,6 +10,9 @@ $search = isset($_GET['search']) ? $_GET['search'] : '';
 $result = display_data($search);
 ?>
 
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,24 +24,71 @@ $result = display_data($search);
     <style>
         body {
             background-color: rgb(224, 183, 20);
-            /* color: black; */
+            color: black;
         }
-        .container {
-            margin-top: 50px;
+        .sidebar {
+            background-color: #333;
+        color: #fff;
+        padding: 20px;
+        height: 100vh;
         }
-        .card {
-            background-color: white;
-            border-radius: 10px;
+        .sidebar .logo {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .sidebar .logo img {
+            width: 80px;
+        }
+        .sidebar ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+        }
+        .sidebar ul li {
+            margin-bottom: 15px;
+        }
+        .sidebar ul li a {
+            color: #fff;
+            text-decoration: none;
+        }
+        .content {
             padding: 20px;
         }
-        .card-header {
-            background-color: rgb(224, 183, 20);
-            color: black;
-            padding: 10px;
-            border-radius: 5px;
+        .navbar {
+            background-color: #000;
+            color: #fff;
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            z-index: 1000;
+        }
+        .navbar .navbar-brand {
+            color: #fff;
+            font-weight: bold;
+        }
+        .navbar .navbar-nav .nav-link {
+            color: #fff;
+            text-decoration: none;
+        }
+        .container {
+            margin-top: 80px;
         }
         .table {
             background-color: white;
+            color: black;
+        }
+        .table thead th {
+            background-color: black;
+            color: yellow;
+            border-color: black;
+            padding: 10px;
+            font-weight: bold;
+            vertical-align: middle;
+        }
+        .table tbody td {
+            border-color: black;
+            padding: 10px;
         }
         .btn-primary {
             background-color: rgb(224, 183, 20);
@@ -60,54 +109,94 @@ $result = display_data($search);
         
     </style>
 </head>
-<body class="bg-dark">
-    <div class="container">
-        <div class="row mt-5">
-            <div class="col">
-                <div class="card mt-5">
-                    <div class="card-header">
-                        <h2 class="display-6 text-center">Pregnant Women Details</h2>
-                    </div>
-                    <div class="card-body">
-                        <form action="" method="GET" class="mb-3">
-                            <div class="input-group">
-                                <input type="text" name="search" class="form-control" placeholder="Search by place" value="<?php echo $search; ?>">
-                                <button type="submit" class="btn btn-primary">Search</button>
+<body>
+    <nav class="navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                
+                Pregnant Women Details
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
+                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 sidebar">
+                <div class="logo">
+                    <img src="your-logo.png" alt="Logo">
+                </div>
+                <ul>
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Settings</a></li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+            </div>
+            <div class="col-md-9 content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <form action="" method="GET" class="mb-3">
+                                <div class="input-group">
+                                    <input type="text" name="search" class="form-control" placeholder="Search by place" value="<?php echo $search; ?>">
+                                    <button type="submit" class="btn btn-primary">Search</button>
+                                </div>
+                            </form>
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Name</th>
+                                            <th>Place</th>
+                                            <th>Pregnancy Due Date</th>
+                                            <th>Contact Information</th>
+                                            <th>Medical History</th>
+                                            <th>Obstetric History</th>
+                                            <th>Blood Type</th>
+                                            <th>Allergies</th>
+                                            <th>Doctor Information</th>
+                                            <!-- <th>Edit</th>
+                                            <th>Delete</th> -->
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+                                            <tr>
+                                                <td><?php echo $row['id']; ?></td>
+                                                <td><?php echo $row['name']; ?></td>
+                                                <td><?php echo $row['place']; ?></td>
+                                                <td><?php echo $row['due_date']; ?></td>
+                                                <td><?php echo $row['contact_info']; ?></td>
+                                                <td><?php echo $row['medical_history']; ?></td>
+                                                <td><?php echo $row['obstetric_history']; ?></td>
+                                                <td><?php echo $row['blood_type']; ?></td>
+                                                <td><?php echo $row['allergies']; ?></td>
+                                                <td><?php echo $row['doctor_info']; ?></td>
+                                                <!-- <td><a href="#" class="btn btn-primary">Edit</a></td>
+                                                <td><a href="#" class="btn btn-danger">Delete</a></td> -->
+                                            </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
                             </div>
-                        </form>
-                        <table class="table table-bordered text-center">
-                            <tr class="table-dark">
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Place</th>
-                                <th>Pregnancy Due Date</th>
-                                <th>Contact Information</th>
-                                <th>Medical History</th>
-                                <th>Obstetric History</th>
-                                <th>Blood Type</th>
-                                <th>Allergies</th>
-                                <th>Doctor Information</th>
-                                <!-- <th>Edit</th>
-                                <th>Delete</th> -->
-                            </tr>
-                            <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-                                <tr>
-                                    <td><?php echo $row['id']; ?></td>
-                                    <td><?php echo $row['name']; ?></td>
-                                    <td><?php echo $row['place']; ?></td>
-                                    <td><?php echo $row['due_date']; ?></td>
-                                    <td><?php echo $row['contact_info']; ?></td>
-                                    <td><?php echo $row['medical_history']; ?></td>
-                                    <td><?php echo $row['obstetric_history']; ?></td>
-                                    <td><?php echo $row['blood_type']; ?></td>
-                                    <td><?php echo $row['allergies']; ?></td>
-                                    <td><?php echo $row['doctor_info']; ?></td>
-                                    <!-- <td><a href="#" class="btn btn-primary">Edit</a></td>
-                                    <td><a href="#" class="btn btn-danger">Delete</a></td> -->
-                                </tr>
-                            <?php } ?>
-                        </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-d6f5/2EnLdIYfqP1IWE3wq75T17yYED8MO4KEnX5bT63JfgIpq62mk1TsTZyS3HZ" crossorigin="anonymous"></script>
+</body>
+</html>
